61bc5bc9cba67cb13bec6111d435bcdf96352640
kkeonwoo <kunwoo1324@naver.com>
2023-08-08 23:42:02 +0900

    blueOrange | works func fin

--- blueOrange/frontLayer/js/popupData.js
+++ blueOrange/frontLayer/js/popupData.js
@@ -1,86 +1,172 @@
-let popupData = [
-    {
-        "id" : 0,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_01.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_01_mo.png",
-    },
-    {
-        "id" : 1,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_02.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_02_mo.png"
-    },
-    {
-        "id" : 2,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_03.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_03_mo.png"
-    },
-    {
-        "id" : 3,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_04.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_04_mo.png"
-    },
-    {
-        "id" : 3,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_05.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_05_mo.png"
-    },
-    {
-        "id" : 3,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_06.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_06_mo.png"
-    },
-    {
-        "id" : 4,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_07.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_07_mo.png"
-    },
-    {
-        "id" : 5,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_08.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_08_mo.png"
-    },
-    {
-        "id" : 6,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_09.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_09_mo.png"
-    },
-    {
-        "id" : 7,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_10.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_10_mo.png"
-    },
-    {
-        "id" : 8,
-        "cntCate": "work_0",
-        "src" : "../frontLayer/images/img/popup/financial/popup_11.png",
-        "mobSrc" : "../frontLayer/images/img/popup/financial/popup_11_mo.png"
-    },
-    {
-        "id" : 0,
-        "cntCate": "work_1",
-        "src" : "../frontLayer/images/img/popup/app/popup_01.png",
-        "mobSrc" : "../frontLayer/images/img/popup/app/popup_01_mo.png"
-    },
-    {
-        "id" : 1,
-        "cntCate": "work_1",
-        "src" : "../frontLayer/images/img/popup/app/popup_02.png",
-        "mobSrc" : "../frontLayer/images/img/popup/app/popup_02_mo.png"
-    },
-    {
-        "id" : 2,
-        "cntCate": "work_1",
-        "src" : "../frontLayer/images/img/popup/app/popup_03.png",
-        "mobSrc" : "../frontLayer/images/img/popup/app/popup_03_mo.png"
-    },
-]
+let popupData = {
+    finance : [
+        {
+            "id" : 0,
+            "src" : "../frontLayer/images/img/popup/financial/popup_01.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_01_mo.png",
+        },
+        {
+            "id" : 1,
+            "src" : "../frontLayer/images/img/popup/financial/popup_02.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_02_mo.png"
+        },
+        {
+            "id" : 2,
+            "src" : "../frontLayer/images/img/popup/financial/popup_03.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_03_mo.png"
+        },
+        {
+            "id" : 3,
+            "src" : "../frontLayer/images/img/popup/financial/popup_04.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_04_mo.png"
+        },
+        {
+            "id" : 3,
+            "src" : "../frontLayer/images/img/popup/financial/popup_05.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_05_mo.png"
+        },
+        {
+            "id" : 3,
+            "src" : "../frontLayer/images/img/popup/financial/popup_06.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_06_mo.png"
+        },
+        {
+            "id" : 4,
+            "src" : "../frontLayer/images/img/popup/financial/popup_07.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_07_mo.png"
+        },
+        {
+            "id" : 5,
+            "src" : "../frontLayer/images/img/popup/financial/popup_08.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_08_mo.png"
+        },
+        {
+            "id" : 6,
+            "src" : "../frontLayer/images/img/popup/financial/popup_09.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_09_mo.png"
+        },
+        {
+            "id" : 7,
+            "src" : "../frontLayer/images/img/popup/financial/popup_10.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_10_mo.png"
+        },
+        {
+            "id" : 8,
+            "src" : "../frontLayer/images/img/popup/financial/popup_11.png",
+            "mobSrc" : "../frontLayer/images/img/popup/financial/popup_11_mo.png"
+        },
+    ],
+    commerce : [
+        {
+            "id" : 0,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_01.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_01_mo.png"
+        },
+        {
+            "id" : 1,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_02.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_02_mo.png"
+        },
+        {
+            "id" : 2,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_03.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_03_mo.png"
+        },
+        {
+            "id" : 3,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_04.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_04_mo.png"
+        },
+        {
+            "id" : 4,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_05.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_05_mo.png"
+        },
+        {
+            "id" : 5,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_06.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_06_mo.png"
+        },
+        {
+            "id" : 7,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_07.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_07_mo.png"
+        },
+        {
+            "id" : 8,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_08.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_08_mo.png"
+        },
+        {
+            "id" : 9,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_09.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_09_mo.png"
+        },
+        {
+            "id" : 11,
+            "src" : "../frontLayer/images/img/popup/commerce/popup_10.png",
+            "mobSrc" : "../frontLayer/images/img/popup/commerce/popup_10_mo.png"
+        },
+    ],
+    app : [
+        {
+            "id" : 0,
+            "src" : "../frontLayer/images/img/popup/app/popup_01.png",
+            "mobSrc" : "../frontLayer/images/img/popup/app/popup_01_mo.png"
+        },
+        {
+            "id" : 1,
+            "src" : "../frontLayer/images/img/popup/app/popup_02.png",
+            "mobSrc" : "../frontLayer/images/img/popup/app/popup_02_mo.png"
+        },
+        {
+            "id" : 2,
+            "src" : "../frontLayer/images/img/popup/app/popup_03.png",
+            "mobSrc" : "../frontLayer/images/img/popup/app/popup_03_mo.png"
+        },
+    ],
+    travel : [
+        {
+            "id" : 0,
+            "src" : "../frontLayer/images/img/popup/travel/popup_01.png",
+            "mobSrc" : "../frontLayer/images/img/popup/travel/popup_01_mo.png"
+        },
+        {
+            "id" : 1,
+            "src" : "../frontLayer/images/img/popup/travel/popup_02.png",
+            "mobSrc" : "../frontLayer/images/img/popup/travel/popup_02_mo.png"
+        },
+        {
+            "id" : 2,
+            "src" : "../frontLayer/images/img/popup/travel/popup_03.png",
+            "mobSrc" : "../frontLayer/images/img/popup/travel/popup_03_mo.png"
+        },
+        {
+            "id" : 3,
+            "src" : "../frontLayer/images/img/popup/travel/popup_04.png",
+            "mobSrc" : "../frontLayer/images/img/popup/travel/popup_04_mo.png"
+        },
+        {
+            "id" : 4,
+            "src" : "../frontLayer/images/img/popup/travel/popup_05.png",
+            "mobSrc" : "../frontLayer/images/img/popup/travel/popup_05_mo.png"
+        },
+    ],
+    etc : [
+        {
+            "id" : 0,
+            "src" : "../frontLayer/images/img/popup/etc/popup_01.png",
+            "mobSrc" : "../frontLayer/images/img/popup/etc/popup_01_mo.png"
+        },
+        {
+            "id" : 1,
+            "src" : "../frontLayer/images/img/popup/etc/popup_02.png",
+            "mobSrc" : "../frontLayer/images/img/popup/etc/popup_02_mo.png"
+        },
+        {
+            "id" : 6,
+            "src" : "../frontLayer/images/img/popup/etc/popup_03.png",
+            "mobSrc" : "../frontLayer/images/img/popup/etc/popup_03_mo.png"
+        },
+    ]
+}
