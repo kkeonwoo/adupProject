57c61e6f12b43e4c1c621ea71a0d1906ae1a05b2
Lee keonwoo <kunwoo1324@naver.com>
2023-09-22 18:21:37 +0900

    gsap

--- advanced-start/03.Advenced Stagger/3-5.3D 텍스트 효과/main.js
+++ advanced-start/03.Advenced Stagger/3-5.3D 텍스트 효과/main.js
@@ -1,24 +1,33 @@
 const tl = gsap.timeline();
 
 const duration = 0.5;
+const numberOfTargets = gsap.utils.toArray('.utils > div').length;
+const pause = 1.5;
+const stagger = duration + pause; // each가 duration보다 크면 차이만큼 pause 되는 시간 발생
+const delay = (stagger * (numberOfTargets - 1)) + pause;
+// pause, duration을 따로 작성하면 유지보수, 디테일한 설정 가능
 
 gsap.set('.utils > div', { transformOrigin: '50% 50% -50'})
 
 tl.from('.utils > div', {
     rotationX: -90,
     opacity: 0,
+    duration: duration,
     stagger: {
-        each: duration,
+        each: stagger,
         repeat: -1,
+        repeatDelay: delay // repeatDelay = duration * (numberOfTargets - 1)
     },
 })
 .to('.utils > div', {
     rotationX: 90,
     opacity: 0,
+    duration: duration,
     stagger: {
-        each: duration,
+        each: stagger,
         repeat: -1,
+        repeatDelay: delay
     }
-}, duration)
+}, stagger)
 
 
--- advanced-start/03.Advenced Stagger/3-8.practice/main.js
+++ advanced-start/03.Advenced Stagger/3-8.practice/main.js
@@ -13,13 +13,91 @@ const planet = [
   'pluto'
 ]
 
+const space = $('.space');
+const list = planet.map((t, i) => {
+  return `<div class="solar_system" data-planet-name="${t}">
+    <div class="planet ${t}">
+      <h2>${t}</h2>
+    </div>
+  </div>`
+})
 
+list.forEach((planet) => {
+  space.insertAdjacentHTML('beforeend', planet);
+})
 
+const z = gsap.utils.distribute({
+  base: -18400,
+  amount: 18400
+})
+
+gsap.set('.planet', {
+  z: z,
+  // scaleX: 0.8,
+  rotateX: 4,
+})
+
+let count = 0;
+let trigger = false;
+const rightButton = $('.right');
+
+rightButton.addEventListener('click', () => {
+  if(count > 7) return;
+  
+  if(!trigger) {
+    ++count;
+
+    gsap.to('.planet', {
+      z: "+=2300", // += 현재 가지고 있는 값에서 value만큼 계산
+      ease: 'power3.inOut',
+      duration: 2,
+      onComplete() {
+        trigger = false;
+      }
+    })
+    trigger = true;
+  }
+})
+
+const leftButton = $('.left');
+
+leftButton.addEventListener('click', () => {
+  if(count < 1) return;
+  
+  if(!trigger) {
+    --count;
+
+    gsap.to('.planet', {
+      z: "-=2300", // += 현재 가지고 있는 값에서 value만큼 계산
+      ease: 'power3.inOut',
+      duration: 2,
+      onComplete() {
+        trigger = false;
+      }
+    })
+    trigger = true;
+  }
+})
 
 // map  -> 배열의 능력 (새로운 배열을 반환)
-// forEach -> 배열의 능력 (값을 반환하지 x)
-// insertAdjacentHTML (dom 뿌려주는)
+// const todo = [
+//   '밥먹기',
+//   '일하기',
+//   '게임하기',
+//   '코딩하기'
+// ]
 
+// const list = todo.map((item, index) => {
+//   return `<div class="${item}" data-index="${index}">${item}</div>`;
+// });
+
+// console.log( list );
+
+// forEach -> 배열의 능력 (값을 반환하지 x)
+// list.forEach((item, index) => {
+//   console.log(item, index);
+// })
+// insertAdjacentHTML (dom 뿌려주는)
 
 
 
