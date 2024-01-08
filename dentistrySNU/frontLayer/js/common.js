0b97ee4a252067b18de64fbb3b5de4c02704cf78
DESKTOP-JT5CO9U\ADUP <kunwoo1324@naver.com>
2024-01-08 14:58:59 +0900

    dentistrySNU | 초기 세팅

--- dentistrySNU/frontLayer/js/common.js
+++ dentistrySNU/frontLayer/js/common.js
@@ -139,129 +139,149 @@ dentistrySNU = {
         lastSelected : null,
         init() {
             const selectBox = $('.form_select');
-            let selectBtn = selectBox.find('.select_btn'),
-                optionArea = selectBox.find('.option_area'),
-                optionItem = selectBox.find('.option_item');
-            const optionBtn = optionArea.find('.option_btn');
-
+            let selectBtn = selectBox.find('.form_btn'),
+                optionArea = $('body > .option_area, .form_select .option_area'),
+                optionItem = optionArea.find('.option_item');
             /**
-             * selectBox 초기값, 모달 내부 위치 시
+             * selectBox 모달 내부 위치 시
              */
             selectBox.each((i, t) => {
-                let optionItem = $(t).find('.option_item'),
-                    optionArea = $(t).find('.option_area'),
+                let firstOption = $(t).find('.option_item').eq(0),
                     tParents = $(t).parents();
 
-                this.handleBtnText(optionItem[0]); // 초기값
+                dentistrySNU.select.handleBtnText(firstOption);
                 // 부모 요소에 스크롤 이벤트 발생 시
                 if (tParents.css('overflow') === 'visible' || tParents.css('overflow') === 'auto') {
-                    tParents.on('scroll', function() { setTimeout(() => fn.hasClass(optionArea, 'show') && this.initBeforeSelect(optionItem), 100); })
+                    tParents.on('scroll', function() { setTimeout(() => dentistrySNU.select.closeOption()); })
                 }
             })
 
             /**
              * selectBox open 시, 위치 및 형제 요소 닫기
              */
-            selectBtn.on('click', (e) => {
-                let t = e.currentTarget,
-                    windowHt = $(window).outerHeight() + $(window).scrollTop(), // 화면 하단 높이
-                    posSelectBtn = $(t).outerHeight() + $(t).offset().top, // select 버튼 하단 높이
-                    selectBtnHt = $(t).outerHeight(),
-                    gap = windowHt - posSelectBtn,
-                    optionArea = $(t).closest('.form_select').find('.option_area'),
-                    optionItem = $(t).closest('.form_select').find('.option_item'),
-                    optionAreaHt = optionArea.outerHeight();
+            selectBtn.on({
+                click: (e) => {
+                    let t = e.currentTarget,
+                        formSelect = $(t).closest('.form_select'),
+                        optionArea = formSelect.find('.option_area'),
+                        optionItem = formSelect.find('.option_item');
 
-                if (!fn.hasClass(optionArea, 'show')) {
-                    selectBtn.attr('aria-expanded', false);
-                    $('.option_area').removeClass('show');
+                    if (!fn.hasClass(formSelect, 'show')) {
+                        $(t).addClass('active');
+                        formSelect.addClass('show');
+                        $('body > .option_area').remove();
+                        $('body').append(`${optionArea.prop('outerHTML')}`);
 
-                    // option 열기
-                    $(t).attr('aria-expanded', true);
-                    if (gap < optionAreaHt) { // 화면과 select 하단의 차이가 optionArea 높이보다 작다면
-                        optionArea.addClass('top');
-                        optionArea.addClass('show').css('top', -optionAreaHt);
+                        optionArea = $('body > .option_area');
+                        optionItem = optionArea.find('.option_item');
+                        
+                        this.handlePosOption(t);
+                        this.lastSelected = optionItem.filter((idx, item) => fn.hasClass(item, 'selected') && item);
+                        return this.lastSelected; // tracking selected item to opening option
                     } else {
-                        optionArea.removeClass('top');
-                        optionArea.addClass('show').css('top', selectBtnHt);
+                        this.closeOption();
                     }
-                    this.lastSelected = optionItem.filter((idx, item) => fn.hasClass(item, 'selected') && item);
-                    return this.lastSelected; // tracking selected item to opening option
-                } else {
-                    this.closeOption();
-                }
-            });
+                },
+                keydown: (e) => {
+                    let t = e.currentTarget,
+                        tOptionItem = $(t).siblings().find('.option_item'),
+                        tActiveItem = $(t).siblings().find('.selected'),
+                        optionItem = $(document).find('body > .option_area .option_item'),
+                        activeIdx = tActiveItem.index();
 
-            /**
-             * selectBox 키보드 제어
-             */
-            selectBtn.on('keydown', (e) => {
-                let t = e.currentTarget,
-                    optionItem = $(t).closest('.form_select').find('.option_item'),
-                    activeItem = $(t).closest('.form_select').find('.selected'),
-                    activeIdx = activeItem.index();
-                
-                if (e.keyCode === 38) {
-                    // up
-                    e.preventDefault();
-                    if (activeIdx <= 0) return;
-                    this.handleSelectOption(optionItem, optionItem.eq(activeIdx - 1));
-                    this.handleBtnText(optionItem.eq(activeIdx - 1));
-                } else if (e.keyCode === 40) {
-                    // down
-                    e.preventDefault();
-                    if (activeIdx >= optionItem.length - 1) return;
-                    this.handleSelectOption(optionItem, optionItem.eq(activeIdx + 1));
-                    this.handleBtnText(optionItem.eq(activeIdx + 1));
-                } else if (e.keyCode === 9 || e.keyCode === 27) {
-                    // escape
-                    fn.hasClass(optionArea, 'show') && this.initBeforeSelect(optionItem);
+                    if (e.keyCode === 38 && activeIdx <= 0) return;
+                    if (e.keyCode === 40 && activeIdx >= tOptionItem.length - 1) return;
+                    if (e.keyCode === 38 || e.keyCode === 40) {
+                        e.preventDefault();
+                        $(t).addClass('active');
+                        $(t).closest('.form_select').addClass('active');
+                        this.handleSelectOption(tOptionItem, tOptionItem.eq(e.keyCode === 38 ? activeIdx - 1 : activeIdx + 1));
+                        this.handleSelectOption(optionItem, optionItem.eq(e.keyCode === 38 ? activeIdx - 1 : activeIdx + 1));
+                        this.handleBtnText(tOptionItem.eq(e.keyCode === 38 ? activeIdx - 1 : activeIdx + 1));
+                    } else if (e.keyCode === 9 || e.keyCode === 27) {
+                        // escape
+                        this.closeOption();
+                    }
                 }
             });
 
             /**
              * option 선택 시
              */
-            optionBtn.on('click', (e) => {
+            $(document).on('click', 'body > .option_area .option_btn', (e) => {
                 let t = e.currentTarget,
-                    optionItem = $(t).closest('.option_item');
+                    optionItem = $('.form_select.show').find('.option_item'),
+                    activeIdx = $(t).closest('.option_item').index(),
+                    selectBtn = $('.form_select.active').find('.form_btn');
 
+                selectBtn.addClass('active');
+                this.handleSelectOption(optionItem, optionItem.eq(activeIdx));
+                this.handleBtnText(optionItem.eq(activeIdx));
                 this.closeOption();
-                this.handleSelectOption(optionItem.siblings(), optionItem);
-                this.handleBtnText(optionItem);
             });
 
             /**
              * 화면 컨트롤 시, select box 닫기
              */
             $(window).on({
-                scroll: function(e) {
-                    let optionArea = $(e.target).find('.option_area');
-
-                    if (fn.hasClass(optionArea, 'show')) dentistrySNU.select.initBeforeSelect(optionItem);
-                },
                 click: function(e) {
                     !$(e.target).closest('.form_select').length && dentistrySNU.select.closeOption();
                 },
                 touchmove: function(e) {
                     !$(e.target).closest('.form_select').length && dentistrySNU.select.closeOption();
+                },
+                resize: function() {
+                    let formSelect = $('.form_select.show'),
+                        formSelectWdh = fn.hasClass(formSelect, 'show') && formSelect.outerWidth(),
+                        posXSelectBtn = fn.hasClass(formSelect, 'show') && formSelect.offset().left,
+                        optionArea = $('body > .option_area');
+
+                        if (fn.hasClass(optionArea, 'show')) optionArea.css({'width' : formSelectWdh, 'left': posXSelectBtn})
+                },
+                scroll: function() {
+                    fn.hasClass('.form_select', 'show') && dentistrySNU.select.closeOption();
                 }
             })
         },
         /**
+         * option 위치 설정
+         * @param {t} target
+         */
+        handlePosOption(t) {
+            let windowHt = $(window).outerHeight() + $(window).scrollTop(), // 화면 하단 높이
+                posXSelectBtn = $(t).offset().left,
+                posYSelectBtn = $(t).outerHeight() + $(t).offset().top, // select 버튼 하단 높이
+                selectBtnWdh = $(t).outerWidth(),
+                selectBtnHt = $(t).outerHeight(),
+                gap = windowHt - posYSelectBtn,
+                optionArea = $('body > .option_area'),
+                optionAreaHt = optionArea.outerHeight(),
+                posYOptionArea = posYSelectBtn - (optionAreaHt + selectBtnHt);
+
+            // option 열기
+            if (gap < optionAreaHt) { // 화면과 select 하단의 차이가 optionArea 높이보다 작다면
+                optionArea.addClass('top');
+                optionArea.addClass('show').css({'top': posYOptionArea, 'left': posXSelectBtn, 'width': selectBtnWdh});
+            } else {
+                optionArea.removeClass('top');
+                optionArea.addClass('show').css({'top': posYSelectBtn, 'left': posXSelectBtn, 'width': selectBtnWdh});
+            }
+        },
+        /**
          * selectBtn 텍스트 값 변경
-         * @param {*} t target
+         * @param {t} target
          */
         handleBtnText(t) {
-            let selectedText = $(t).find('button').text(),
-                selectBtn = $(t).closest('.form_select').find('.select_btn');
-            
-            selectBtn.text(selectedText);
+            let selectedText = $(t).find('.option_btn').text(),
+                selectedIdx = $(t).closest('.option_item').index(),
+                selectBtn = $(t).closest('.form_select').find('.form_btn');
+
+            selectBtn.text(selectedText).attr('selected-idx', selectedIdx);
         },
         /**
          * 선택된 option class, attr 변경
-         * @param {*} initObj 변경 전
-         * @param {*} activeObj 변경 후
+         * @param {initObj}  변경 전
+         * @param {activeObj}  변경 후
          */
         handleSelectOption(initObj, activeObj) {
             $(initObj).removeClass('selected').attr('aria-selected', false);
@@ -271,20 +291,12 @@ dentistrySNU = {
          * select option list 닫기
          */
         closeOption() {
+            $('.form_select').removeClass('show');
             $('.form_select').find('.select_btn').attr('aria-expanded', false);
             $('.form_select').find('.option_area').removeClass('show top');
+            $('body > .option_area').remove();
         },
-        /**
-         * 선택하지 않고 닫았을 경우, 이전 선택값(초기값)으로 돌아가기
-         * @param {*} t target
-         */
-        initBeforeSelect(t) {
-            this.closeOption();
-            $(t).removeClass('selected').attr('aria-selected', false);
-            this.lastSelected.addClass('selected').attr('aria-selected', true);
-            this.handleBtnText(this.lastSelected);
-        },
-    }
+    },
 }
 $(() => {
     dentistrySNU.init();
