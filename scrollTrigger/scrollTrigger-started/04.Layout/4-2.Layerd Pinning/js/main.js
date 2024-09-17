694e9dfd4b11072e2f7e1c1cff7df20fac8ce989
DESKTOP-JT5CO9U\ADUP <kunwoo1324@naver.com>
2024-09-17 21:42:35 +0900

    scrolltrigger study

--- scrollTrigger/scrollTrigger-started/04.Layout/4-2.Layerd Pinning/js/main.js
+++ scrollTrigger/scrollTrigger-started/04.Layout/4-2.Layerd Pinning/js/main.js
@@ -1,5 +1,21 @@
-
-
+gsap.utils.toArray('.section').forEach((section, index) => {
+    
+    ScrollTrigger.create({
+        trigger: section,
+        start: 'top top',
+        // end: 'bottom center',
+        // animation: ,
+        pin: true,
+        pinSpacing: false,
+        markers: true,
+        scrub: true,
+        snap: {
+            snapTo: 1,
+            duration: 1,
+            ease: "power2.inOut"
+        },
+    })
+});
 
 
 
