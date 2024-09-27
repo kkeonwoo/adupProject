dec054cdb7552edc3eff4ebafe3c0592dc0ad72e
kkeonwoo <kunwoo1324@naver.com>
2024-09-28 00:28:37 +0900

    scrolltrigger study

--- scrollTrigger/scrollTrigger-started/04.Layout/4-4.Horizontal Layout/js/markers.js
+++ scrollTrigger/scrollTrigger-started/04.Layout/4-4.Horizontal Layout/js/markers.js
@@ -5,8 +5,14 @@ const markers = () => {
   if (document.querySelector('.gsap-marker-scroller-start')) {
     const markers = gsap.utils.toArray('[class *= "gsap-marker"]');
 
+    const scrollMarkers = markers.filter((m) => m.className.includes('scroller'));
+
     scrollbar.addListener(({ offset }) => {
-      gsap.set(markers, { marginTop: -offset.y });
+      gsap.set(scrollMarkers, { marginLeft: -offset.y }); // 수평방향이니 marginLeft로 변경
+      // start, end marker는 잘 찾지만
+      // scroller-start, scroller-end는 위치를 못찾으니
+      // markers 배열에서 해당 요소만 set을 받도록 세팅
+
     });
   }
 }
